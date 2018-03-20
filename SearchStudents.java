21b3b4bf434da53cffed2b34a1fd25a7f17b4e27
ORLA CONNELLY - STUDENT <G00356556@gmit.ie>
2018-03-20 14:00:33 +0000

    some message regarding lab

---
+++ SearchStudents.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class SearchStudents{
+ public static void main(String[] args){
+ String[][] students ={
+ {"Mary Reilly","Software Development","Year 1", "GMIT"},
+ {"Garret Winters","Business Studies" , "Year 3","AIT"},
+ {"Ashley Cox", "Chemistry", "Year 3", "NUIG"},
+ {"Joe Kelly", "Computing and digital media","Year 2", "GMIT"}
+ };
+
+ Scanner input = new Scanner(System.in);
+ System.out.print("Enter a search value: ");
+ String searchStr = input.nextLine();
+ int match = 0;
+
+ for(int i = 0; i < students.length; i++){
+  for(int j = 0; j < students[i].length; j++){
+   if(searchStr.equals(students[i][j])){
+   match++;
+   if(match == 1){
+   }
+    System.out.println(students[i][0]+"\t"+students[i][3]);
+
+  }
+ }
+}
+System.out.println();
+System.out.println("Records with match: "+match);
+Index.main(null);
+}
+}
---
+++ TwoD_Array.java
@@ -0,0 +1,27 @@
+public class TwoD_Array{
+ public static void main(String[] args){
+  int[][] x = {{1,2},{3,4}};
+
+  System.out.println("Using Enhanced for");
+  for(int[] i: x){
+   for(int j: i)
+     System.out.println(j);
+
+
+    }
+
+    System.out.println();
+
+    System.out.println("Not using Enhanced for");
+    for(int i = 0; i < x.length; i++){
+     for(int j = 0; j <x[i].length; j++)
+     System.out.println(x[i][j]);
+
+
+
+
+
+
+}
+}
+}
